55dccdeea8e35ed520127559d68cea93757f0972
Amirhossein Shahbabaee <amirhossein.shahbabaee@citynetwork.eu>
2023-03-07 12:15:08 +0100

    Marbles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import Board from './components/Board';
 import DropZone from './components/DropZone';
 
 function App() {
 
+  const [turn,setTurn] = useState(2)
+  const [dropped,setDropped] = useState([
+    {x:6, y:0, player:1},
+    {x:6, y:1, player:2},
+    {x:6, y:4, player:1},
+    {x:5, y:4, player:1},
+    {x:4, y:4, player:1},
+  ])
+
   return (
     <div className="App">
-      <DropZone/>
+      <DropZone dropped={dropped} turn={turn}/>
       <Board/>
     </div>
   );
--- src/components/DropZone.js
+++ src/components/DropZone.js
@@ -1,8 +1,14 @@
 import './DropZone.css'
+import { size } from '../constants/constants'
 
-const DropZone = () => {
+const DropZone = ({dropped,turn}) => {
     return <div className="drop-zone">
-        <div/>
+        {dropped.map((m,i) => 
+            <div key={i} 
+                className={`p${m.player}`}
+                style={{transform:`translate(${m.y*size}px,${m.x*size+50}px)`}}>
+            </div>)}
+        <div className={`active p${turn}`}/>
     </div>     
 }
 
